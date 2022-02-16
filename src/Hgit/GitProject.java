7c062261db49a47c57b8a0ccc1e8756fdc4b1c87
Himel <himel7152@gamil.com>
2022-02-16 13:54:53 -0500

    Git Project has added

---
+++ src/Hgit/GitProject.java
@@ -0,0 +1,12 @@
+package Hgit;
+
+public class GitProject {
+
+	public static void main(String[] args) {
+		System.out.println("My Name Is Himel");
+		System.out.println("I am a student of Telentech");
+		System.out.println("I am a softwere tester");
+System.out.println("Himel practice");
+	}
+
+}
